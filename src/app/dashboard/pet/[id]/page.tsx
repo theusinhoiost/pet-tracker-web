255b38f4011f1df22c8ec9632bd4606ca8de7222
Matheus Iost <137854934+theusinhoiost@users.noreply.github.com>
2026-07-28 15:00:32 -0300

    feat: rename component import and update rendering in PetInfosPage

--- src/app/dashboard/pet/[id]/page.tsx
+++ src/app/dashboard/pet/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { getLastWeightsByPetId } from "@/services/api/weight";
-import PetInfosClient from "./pet-infos-client";
+import ChartLineDots from "./pet-infos-client";
 
 interface PetInfosPageProps {
   params: Promise<{
@@ -12,5 +12,5 @@ export default async function PetInfosPage({ params }: PetInfosPageProps) {
 
   const weights = await getLastWeightsByPetId(id);
 
-  return <PetInfosClient petId={id} weights={weights} />;
+  return <ChartLineDots weights={weights} />;
 }
